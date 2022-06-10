db31cffa62b2e007d928d14354b00a23c0076c23
Martin Wögerbauer <20162034@atn.ac.at>
2022-06-10 15:51:42 +0200

    Fixed container issue

--- API/Models/orders.py
+++ API/Models/orders.py
@@ -37,13 +37,13 @@ class Order(BaseModel,Base ):
     #adding sample data to db (only for tesing purposes)
     def add(self):
         order = Order()
-        order.id = 'Ordernumber 104'
-        order.product_id = 'Blasius 9'
+        order.id = 'Order 2'
+        order.product_id = 'Blasius 10'
         order.planParts = 10000
         order.prodParts = 1643
         order.startTime = datetime.now();
         order.deadline = datetime.now() + timedelta(days=2)
-        order.orderStatus = 'Running'
+        order.orderStatus = 'Finished'
         order.workingStation = 'Robot01'
         self.session.add(order)
         self.session.commit()
