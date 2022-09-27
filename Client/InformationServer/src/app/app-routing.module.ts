a092f6cb30a4ba289b626739b386053f8f3efd01
Daniel Wögerbauer <20162034@atn.ac.at>
2022-09-27 16:07:41 +0200

    Added home component

--- Client/InformationServer/src/app/app-routing.module.ts
+++ Client/InformationServer/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { ProductSubPageComponent } from './product-sub-page/product-sub-page.component';
 import { ProductsComponent } from './productInformation/products.component';
 import { RobotsComponent } from './robots/robots.component';
@@ -8,7 +9,8 @@ const routes: Routes = [
   {path: 'robots', component: RobotsComponent},
   {path: 'products', component: ProductsComponent},
   {path: 'products/:id/product-detail', component:ProductSubPageComponent},
-  {path: 'overview', loadChildren: () => import('./overview/overview.module').then(m => m.OverviewModule)}
+  {path: 'overview', loadChildren: () => import('./overview/overview.module').then(m => m.OverviewModule)},
+  {path: '**', component: HomeComponent}
 
   ];
 
